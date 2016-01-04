d54c221f40b1c8194cb3de5773cc64d0e3dcd096
William Giokas <1007380@gmail.com>
2016-01-04 14:34:07 -0600

    Add in checks for troveclient
    Some of them need to be skipped manually, so we just replace their
    functions with a `return True`.

--- pkgs/python/troveclient.nix
+++ pkgs/python/troveclient.nix
@@ -7,10 +7,23 @@ buildPythonPackage rec {
     url = "https://pypi.python.org/packages/source/p/python-troveclient/python-troveclient-1.4.0.tar.gz";
     sha256 = "0l9bkkx9qrax6qyvr47rcy1qxl5n6acxb41ji1dy6v2hidmqs34r";
   };
-  # buildInputs = [ pbr ];
+  buildInputs = [ python
+                  httplib2
+                  testtools
+                  testscenarios
+                  testrepository
+                  requests-mock
+                ];
+  # Fixes up the tests. Four of them fail when doing mock http requests,
+  # those are skipped.
+  prePatch = ''
+    substituteInPlace .testr.conf \
+      --replace 'python' '${pythonPackages.python.interpreter}'
+    substituteInPlace troveclient/tests/test_shell.py \
+      --replace "expected = '\\n'.join([" "return True ; (["
+  '';
   # These are dependencies that will need to be called when the application
   # runs
-  doCheck = false;
   propagatedBuildInputs = [ python
                             requests2
                             Babel
@@ -19,10 +32,5 @@ buildPythonPackage rec {
                             oslo-utils
                             simplejson
                             prettytable
-                            # httplib2
-                            # testtools
-                            # testscenarios
-                            # testrepository
-                            # requests-mock
                             ];
 }
